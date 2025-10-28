978a874a53e5e6149410923f7e1f5761c82a5581
Andrea Itacarambi <deia.bia2@yahoo.com.br>
2025-10-28 14:43:33 +0000

    Atualiza o HTML da página login.php

--- auth/login.php
+++ auth/login.php
@@ -6,56 +6,39 @@
     <title>Login</title>
     <link rel="stylesheet" href="../assets/css/output.css">
 </head>
+
 <body>
     <div class="container">
-        <div class="tabs bg-black text-white">
-            <a href="login.php" class="">Login</a>
-            <a href="registo.php" class="tab">Register</a>
+        <div class="tabs bg-black text-white w-32">
+            <img src="assets/images/logo_optispace" alt="Logo">
+            <img src="assets/images/Saly-2" alt="Decoração">
         </div>
+    </div>
 
-        <form method="POST">
+    <div class="right-section m-5">
+                <div class="form-card">
+                        <img src="../assets/css/output.css" alt="Logo">
+                        <img src="../assets/css/output.css" alt="Decoração">
+                </div>
+    </div>       
+
+    <form method="POST">
                 <label>Insira o seu Email</label>
                 <input type="email" name="email" required placeholder="E-mail">
                         
                 <label>Insira a sua Palavra-passe</label>
                 <input type="password" name="password" required placeholder="Palavra-passe">
-                        
-                <button type="submit">Entrar</button>
-        </form>
-        
-        <div class="content-wrapper">
-            <div class="left-section">
-                <img src="assets/css/output.css" alt="Logo">
 
-            </div>
-            
-            <div class="right-section m-5">
-                <div class="form-card">
-                    <div class="logo">
-                        <img src="../assets/css/output.css" alt="Logo">
-                        <img src="../assets/css/output.css" alt="Decoração">
-                    </div>
-                    
-                    <h1>Bem-vindo à Optispace</h1>
+                <button type="submit">Entrar</button>
+
+                <h1>Bem-vindo à Optispace</h1>
                     <h2>Entrar</h2>
                     
                     <div class="links">
                         <span class="active">Não tem conta?</span>
                         <a href="registo.php">Registe-se</a>
                     </div>
-                    
-                    <?php if ($error): ?>
-                        <div class="error"><?php echo $error; ?></div>
-                    <?php endif; ?>
-                    
-                    
-                </div>
-            </div>
-            
-            <div class="decoration">
-                <img src="../assets/css/computer_inicio" alt="Decoration">
-            </div>
-        </div>
-    </div>
+    </form>
+        
 </body>
 </html>
