96122d137e0a58da7ebd464afd828a6855aeedb4
Nathan-DuarteGIT <duartenathan60@gmail.com>
2025-10-29 10:18:10 +0000

    Adição dos cards da section serviços no index.php e adição da imagem de fundo

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <title>OptiSpace-Homepage</title>
 </head>
 
-<body class="bg-primary ml-40 mr-40 font-sans">
-    <header>
+<body class="bg-primaryfont-sans">
+    <header class="mx-40 ">
         <nav class="flex justify-between items-center ml-8 mr-8 mt-2">
             <img class="w-32 h-auto " src="assets/images/logo_optispace.png" alt="logo_OptiSpace">
             <ul class="flex justify-between items-center">
@@ -22,7 +22,12 @@
             </ul>
         </nav>
     </header>
-    <section id="inicio" class="flex items-center my-20">
+    <!-- SVG decorativo -->
+    <img
+        src="assets/images/bg-element.png"
+        alt=""
+        class="absolute -bottom-[1000px] -left-40 w-[900px] -z-10 opacity-90 select-none pointer-events-none" />
+    <section id="inicio" class="mr-40 ml-40 flex items-center my-20">
         <div class="flex-1">
             <h1 class="text-4xl font-bold">Reserve com inteligência, otimize com Optispace.</h1>
             <h2 class="text-2xl font-light text-description my-10">A Optispace automatiza a gestão de recursos partilhados com reservas inteligentes, eliminando conflitos e aumentando a produtividade empresarial.</h2>
@@ -35,25 +40,38 @@
             <img src="assets/images/pc1.png" alt="um computador">
         </div>
     </section>
-    <section id="servicos" class="flex flex-col justify-center text-center my-20">
+    <section id="servicos" class="mx-40 relative flex flex-col justify-center text-center my-20 overflow-hidden">
+
+
         <h1 class="text-4xl font-bold">Os Nossos Serviços</h1>
         <div class="w-16 h-[2px] bg-black mx-auto my-5"></div>
-        <p class="font-light text-description mx-auto w-3/4">Simplificamos a gestão de recursos partilhados, oferecendo reservas automáticas de salas e equipamentos, otimização em tempo real e rastreio eficiente, eliminando conflitos e aumentando a produtividade da sua equipa</p>
-        <div>
-            <div>
-                <img src="" alt="">
-                <p></p>
-                <p></p>
+        <p class="font-light text-description mx-auto w-3/4">
+            Simplificamos a gestão de recursos partilhados, oferecendo reservas automáticas de salas e equipamentos, otimização em tempo real e rastreio eficiente, eliminando conflitos e aumentando a produtividade da sua equipa
+        </p>
+
+        <div class="flex justify-between my-10">
+            <div class="card">
+                <img src="assets/images/reservas1.png" alt="imagem de reservas">
+                <p class="text-2xl font-bold">Reservas inteligentes</p>
+                <p class="font-light text-description">
+                    Indique participantes e equipamentos que sugerimos a melhor opção em tempo real, integrado com os seus calendários.
+                </p>
             </div>
-            <div>
-                <img src="" alt="">
-                <p></p>
-                <p></p>
+
+            <div class="card">
+                <img src="assets/images/equipamentos1.png" alt="imagem de equipamentos">
+                <p class="text-2xl font-bold">Gestão de recursos</p>
+                <p class="font-light text-description">
+                    Reserve equipamentos, viaturas ou outros materiais de forma simples, rápida e segura.
+                </p>
             </div>
-            <div>
-                <img src="" alt="">
-                <p></p>
-                <p></p>
+
+            <div class="card">
+                <img src="assets/images/relogio.png" alt="imagem de relógio">
+                <p class="text-2xl font-bold">Check-in automático</p>
+                <p class="font-light text-description">
+                    Check-in e check-out rápidos para uma gestão eficiente.
+                </p>
             </div>
         </div>
     </section>
