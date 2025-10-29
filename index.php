a831114397753132acc692e9e28ca4fb942ddc63
Nathan-DuarteGIT <duartenathan60@gmail.com>
2025-10-29 00:14:43 +0000

    alteração de alguns elementos no input.css e conclusão da section inicio e começo da section serviços no index.php

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <title>OptiSpace-Homepage</title>
 </head>
 
-<body class="bg-primary ml-40 mr-40">
+<body class="bg-primary ml-40 mr-40 font-sans">
     <header>
-        <nav class="flex justify-between items-center ml-8 mr-8 mt-0">
-            <img class="w-32 h-32 " src="assets/images/logo_optispace.png" alt="logo_OptiSpace">
+        <nav class="flex justify-between items-center ml-8 mr-8 mt-2">
+            <img class="w-32 h-auto " src="assets/images/logo_optispace.png" alt="logo_OptiSpace">
             <ul class="flex justify-between items-center">
                 <li class="ml-4 mr-4"><a href="">Inicio</a></li>
                 <li class="ml-4 mr-4"><a href="">Serviços</a></li>
@@ -22,20 +22,40 @@
             </ul>
         </nav>
     </header>
-    <section id="inicio" class="flex items-center">
+    <section id="inicio" class="flex items-center my-20">
         <div class="flex-1">
             <h1 class="text-4xl font-bold">Reserve com inteligência, otimize com Optispace.</h1>
-            <h2 class="text-2xl font-light text-description">A Optispace automatiza a gestão de recursos partilhados com reservas inteligentes, eliminando conflitos e aumentando a produtividade empresarial.</h2>
+            <h2 class="text-2xl font-light text-description my-10">A Optispace automatiza a gestão de recursos partilhados com reservas inteligentes, eliminando conflitos e aumentando a produtividade empresarial.</h2>
             <div class="mt-5">
-                <a class="btn-outline" href="#juntese">Comece agora</a>
+                <a class="btn-outline mr-4" href="#juntese">Comece agora</a>
+                <a class="bg-[#17876E] px-10 py-3 rounded-full text-white font-bold hover:bg-[#2EA98C] transition duration-200" href="#servicos">Saiba mais</a>
             </div>
         </div>
         <div class="flex-1">
             <img src="assets/images/pc1.png" alt="um computador">
         </div>
     </section>
-    <section id="servicos">
-
+    <section id="servicos" class="flex flex-col justify-center text-center my-20">
+        <h1 class="text-4xl font-bold">Os Nossos Serviços</h1>
+        <div class="w-16 h-[2px] bg-black mx-auto my-5"></div>
+        <p class="font-light text-description mx-auto w-3/4">Simplificamos a gestão de recursos partilhados, oferecendo reservas automáticas de salas e equipamentos, otimização em tempo real e rastreio eficiente, eliminando conflitos e aumentando a produtividade da sua equipa</p>
+        <div>
+            <div>
+                <img src="" alt="">
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p></p>
+                <p></p>
+            </div>
+            <div>
+                <img src="" alt="">
+                <p></p>
+                <p></p>
+            </div>
+        </div>
     </section>
     <section id="juntese">
 
