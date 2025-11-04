12cc19f1b034d74bdac92f3cf2f808e17575c32e
Andrea Itacarambi <deia.bia2@yahoo.com.br>
2025-11-04 12:43:44 +0000

    estiliza o index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -21,24 +21,24 @@ showvar();
     <title>Dashboard</title>
 </head>
 
-<body>
+<body class="bg-primary font-sans text-gray-900 min-h-screen flex flex-col">
     <?php include '../includes/header.php'; ?>
 
-    <main class="flex-1 p-8">
+    <main class="flex-1 p-8 rounded-2xl shadow-md m-6">
         <section class="mt-8">
             <h3 class="text-2xl font-semibold mb-6">Dashboard</h3>
 
             <!-- Container dos cards -->
             <div class="flex flex-col sm:flex-row gap-6">
 
-                <!-- Card: Salas -->
-                <div class="bg-white w-full sm:w-1/3 px-3 py-2 rounded-xl shadow-md hover:shadow-lg transition flex justify-between items-center">
-                    <div class="leading-none">
-                        <h4 class="text-base font-medium text-gray-700">Salas</h4>
-                        <p id="salas" class="text-xl font-bold text-gray-900 mt-0.5">0</p>
-                    </div>
-                    <img src="../assets/images/salas.png" alt="Salas" class="w-8 h-auto ml-1">
+            <!-- Card: Salas -->
+            <div class="bg-white w-full sm:w-1/3 px-5 py-6 h-44 rounded-2xl border border-gray-200 shadow-lg flex items-center justify-between">
+                <div class="leading-tight">
+                <h4 class="text-base font-semibold text-gray-700">Salas</h4>
+                <p id="salas" class="text-4xl font-black text-gray-900 mt-1">0</p>
                 </div>
+                <img src="../assets/images/salas.png" alt="Salas" class="w-10 h-auto ml-1">
+            </div>
 
                 <!-- Card: Equipamentos -->
                 <div class="bg-white w-full sm:w-1/3 px-3 py-2 rounded-xl shadow-md hover:shadow-lg transition flex justify-between items-center">
