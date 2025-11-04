e0f74fa2ae264481161b09014fdea20669c94238
Andrea Itacarambi <deia.bia2@yahoo.com.br>
2025-11-04 10:50:50 +0000

    Cria a dashboard

---
+++ dashboard/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html lang="pt" class="h-screen w-screen">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/output.css">
+    <link rel="shortcut icon" href="../assets/images/logo_optispace.ico" type="image/x-icon">
+    <title>Dashboard</title>
+</head>
+
+<body class=">
+    
+
+</body>
