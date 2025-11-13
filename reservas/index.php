ae09bb3579c1ce22337432d35f91499146c27e4d
Andrea Itacarambi <deia.bia2@yahoo.com.br>
2025-11-13 23:07:27 +0000

    cria a estilização da página principal do reservas

--- reservas/index.php
+++ reservas/index.php
@@ -26,6 +26,54 @@ include_once '../includes/functions.php';
         <!-- Conteúdo principal -->
 
         <main class="flex-1 p-8">
+            <section class="mt-8">
+                <h3 class="text-2xl font-semibold mb-6 text-black">Reservas</h3>
+
+                <!-- Container dos cards -->
+                <div class="flex flex-col sm:flex-row justify-start gap-6">
+                    <!-- card criar reserva -->
+                    <div class="card-dashboard">
+                        <div class="leading-tight">
+                            <h4 class="text-lg font-bold text-black">Criar Reserva</h4>
+                        </div>
+                    </div>
+
+                    <div class="card-dashboard">
+                        <div class="leading-tight">
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Data:</span> 11/10/2025</p>
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Hora de início:</span> 10:00</p>
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Hora de fim:</span> 14:00</p>
+                            <p class="text-sm text-black mb-2"><span class="font-medium">Recurso:</span> Sala 2</p>
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Status:</span></p>
+                            <span class="inline-block bg-green-200 text-green-800 text-xs font-medium px-3 py-1 rounded">confirmada</span>
+                        </div>
+                    </div>
+
+                    <div class="card-dashboard ">
+                        <div class="leading-tight">
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Data:</span> 10/10/2025</p>
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Hora de início:</span> 09:30</p>
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Hora de fim:</span> 17:30</p>
+                            <p class="text-sm text-black mb-2"><span class="font-medium">Recurso:</span> Sala 4</p>
+                            <p class="text-sm text-black mb-1"><span class="font-medium">Status:</span></p>
+                            <span class="inline-block bg-green-200 text-green-800 text-xs font-medium px-3 py-1 rounded">confirmada</span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-col sm:flex-row justify-start gap-8 mt-6">
+                        <div class="bg-white w-64 h-auto px-4 py-3 rounded-2xl border border-gray-200">
+                            <p class="text-xs text-black leading-relaxed"><span class="font-semibold">Data:</span> 05/10/2025</p>
+                            <p class="text-xs text-black leading-relaxed"><span class="font-semibold">Hora de início:</span> 8:20</p>
+                            <p class="text-xs text-black leading-relaxed"><span class="font-semibold">Hora de fim:</span> 10:20</p>
+                            <p class="text-xs text-black leading-relaxed"><span class="font-semibold">Recurso:</span> sala 3</p>
+                            <div class="mt-2">
+                                <p class="text-xs text-black mb-1"><span class="font-semibold">Status:</span></p>
+                                <span class="inline-block bg-red-200 text-red-800 text-xs font-medium px-3 py-1 rounded">cancelada</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     </div>
 </body>
