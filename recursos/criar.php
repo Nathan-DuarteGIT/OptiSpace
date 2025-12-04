669791a3012c4a38ecd675e2e2ff7ec7c7e53f9a
NathanD <duartenathan60@gmail.com>
2025-12-04 15:40:01 +0000

    teste

--- recursos/criar.php
+++ recursos/criar.php
@@ -64,7 +64,7 @@ require_once "../config/config.php";
                         <div>
                             <label class="block text-sm font-medium text-gray-700 mb-2">Equipamentos (opcional)</label>
                             <div class="space-y-3">
-                                <?php render_equipamentos_fixos_formCriar($_SESSION['user_id'])?>
+                                <?php //render_equipamentos_fixos_formCriar($_SESSION['user_id'])?>
                                 <label class="flex items-center cursor-pointer">
                                     <input type="checkbox" name="equipamentos_sala[]" value="computador" class="mr-3 w-4 h-4 text-indigo-600 rounded focus:ring-2 focus:ring-indigo-500">
                                     <span>Computador</span>
