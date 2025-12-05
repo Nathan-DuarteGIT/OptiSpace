bd96dae6e420a4f6fc519ee594d12509df2455ac
NathanD <duartenathan60@gmail.com>
2025-12-05 15:37:11 +0000

    primeira tentativa de criar uma viatura

--- actions/action-criarRecurso.php
+++ actions/action-criarRecurso.php
@@ -15,7 +15,7 @@
                 
 
             }else if($tipo_recurso === 'equipamento'){
-                $caminho_imagem = "../uploads/recurso_default.png";
+                $caminho_imagem = "../uploads/recurso-default.png";
 
                 if(isset($_FILES['imagem_equipamento'])) {
                     $upload_result = upload_imagem($_FILES['imagem_equipamento'], '../uploads');
@@ -51,6 +51,44 @@
                 }
 
             }else if($tipo_recurso === 'viatura'){
+                $caminho_imagem = "../uploads/viatura-default.png";
+
+                if(isset($_FILES['imagem_viatura'])) {
+                    $upload_result = upload_imagem($_FILES['imagem_viatura'], '../uploads');
+                    if($upload_result['sucesso']) {
+                        $caminho_imagem = $upload_result['caminho'];
+                    } else {
+                        header("Location: " . BASE_URL . "recursos/criar.php?erro_upload=" . urlencode($upload_result['mensagem']));
+                        exit();
+                    }
+                }
+
+                if(isset($_POST['marca'], $_POST['modelo'], $_POST['matricula'])) {
+                    $marca = $_POST['marca'];
+                    $modelo = $_POST['modelo'];
+                    $matricula = $_POST['matricula'];
+                    $empresa_id = buscar_empresa($_SESSION['user_id']);
+
+                    $stmt = $conn->prepare("INSERT INTO equipamentos (empresa_id, nome, matricula, modelo, marca, foto_path) VALUES (:empresa_id, :nome, :matricula, :modelo, :marca, :caminho)");
+                    $stmt->bindParam(':empresa_id', $empresa_id);
+                    $stmt->bindParam(':nome', $nome_recurso);
+                    $stmt->bindParam(':matricula', $matricula);
+                    $stmt->bindParam(':modelo', $modelo);
+                    $stmt->bindParam(':marca', $marca);
+                    $stmt->bindParam(':caminho', $caminho_imagem);
+                    
+                    
+                    if($stmt->execute()) {
+                        header("Location: " . BASE_URL . "recursos/index.php?sucesso=" . urlencode("Recurso criado com sucesso."));
+                        exit();
+                    } else {
+                        header("Location: " . BASE_URL . "recursos/criar.php?erro_db=" . urlencode("Erro ao criar recurso. Tente novamente."));
+                        exit();
+                    }
+                } else {
+                    header("Location: " . BASE_URL . "recursos/criar.php?erro_campos=" . urlencode("Por favor, selecione o tipo de equipamento."));
+                    exit();
+                }
 
             }
 
