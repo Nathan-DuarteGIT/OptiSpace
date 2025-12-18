589d2f475453a6582dae45ca38b1641cf454c4e2
NathanD <duartenathan60@gmail.com>
2025-12-18 11:48:36 +0000

    teste de controle da versão do css

--- reservas/criar.php
+++ reservas/criar.php
@@ -3,6 +3,8 @@ include_once '../includes/functions.php';
 require_once "../config/config.php";
 $file_path = '../assets/js/reservas.js';
 $version = filemtime($file_path);
+$file_css = '../assets/css/output.css';
+$versioncss = filemtime($file_css);
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +13,7 @@ $version = filemtime($file_path);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/output.css?v=<?= $version ?>">
+    <link rel="stylesheet" href="../assets/css/output.css?v=<?= $versioncss ?>">
     <link rel="shortcut icon" href="../assets/images/logo_optispace.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
