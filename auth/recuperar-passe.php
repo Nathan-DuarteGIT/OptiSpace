5d2f718f9771fb03ac43c87567724a03494a6809
Andrea Itacarambi <deia.bia2@yahoo.com.br>
2025-12-18 11:58:29 +0000

    Atualiza o caminho para a nova versão do Tailwind no auth/recuperar-passe.php

--- auth/recuperar-passe.php
+++ auth/recuperar-passe.php
@@ -1,5 +1,7 @@
 <?php
 require_once "../config/config.php";
+$file_css = '../assets/css/output.css';
+$versioncss = filemtime($file_css);
 ?>
 <!DOCTYPE html>
 
@@ -8,7 +10,7 @@ require_once "../config/config.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/output.css">
+    <<link rel="stylesheet" href="../assets/css/output.css?v=<?= $versioncss ?>">
     <link rel="shortcut icon" href="../assets/images/logo_optispace.ico" type="image/x-icon">
     <title>Recuperar passe</title>
 </head>
